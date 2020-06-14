72de25e3b319ef8927319fc9fb79aa583fbe0e9d
Aidan Brady <me@aidancbrady.com>
2020-06-14 00:02:09 -0400

    Implement loading page, background images & about page

--- app/src/main/java/com/aidancbrady/sandyspringsconservancy/MainActivity.java
+++ app/src/main/java/com/aidancbrady/sandyspringsconservancy/MainActivity.java
@@ -15,7 +15,10 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.aidancbrady.sandyspringsconservancy.core.DataCache;
 import com.aidancbrady.sandyspringsconservancy.core.DataHandler;
@@ -29,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // use handler to run callback on main thread
+        Handler handler = new Handler(getMainLooper());
+        new Thread(() -> {
+            boolean ret = new DataHandler(this).loadData();
+            handler.post(() -> callback(ret));
+        }).start();
+    }
+
+    private void callback(boolean success) {
+        ProgressBar progress = findViewById(R.id.progressBar);
+        TextView view = findViewById(R.id.textView);
+        progress.setVisibility(View.GONE);
+        if (success) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        } else {
+            view.setText(R.string.park_list_download_fail);
+        }
     }
 
     public void onClick(View view) {
--- app/src/main/java/com/aidancbrady/sandyspringsconservancy/ui/HomeFragment.java
+++ app/src/main/java/com/aidancbrady/sandyspringsconservancy/ui/HomeFragment.java
@@ -1,28 +1,36 @@
 package com.aidancbrady.sandyspringsconservancy.ui;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
 import com.aidancbrady.sandyspringsconservancy.R;
 import com.aidancbrady.sandyspringsconservancy.core.Constants;
+import com.aidancbrady.sandyspringsconservancy.core.DataCache;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private ImageView background;
+    private int backgroundIndex = 0;
+
+    private Timer backgroundTimer;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         root.findViewById(R.id.map_button)
                 .setOnClickListener(v -> open(v, R.id.nav_park_map));
@@ -36,7 +44,37 @@ public class HomeFragment extends Fragment {
                     intent.setData(Uri.parse(Constants.WEBSITE));
                     startActivity(intent);
                 });
+
+        background = root.findViewById(R.id.backgroundImage);
+        background.setImageBitmap(DataCache.backgroundList.get(0));
+
+        if (backgroundTimer == null) {
+            backgroundTimer = new Timer();
+            runBackgroundAnimation();
+        }
+
         return root;
+    }
+
+    private void runBackgroundAnimation() {
+        backgroundTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                int prev = backgroundIndex;
+                backgroundIndex = (backgroundIndex + 1) % DataCache.backgroundList.size();
+                Drawable curDrawable = new BitmapDrawable(getResources(), DataCache.backgroundList.get(prev));
+                Drawable nextDrawable = new BitmapDrawable(getResources(), DataCache.backgroundList.get(backgroundIndex));
+                Drawable[] arr = new Drawable[] {curDrawable, nextDrawable};
+                TransitionDrawable transition = new TransitionDrawable(arr);
+                background.setImageDrawable(transition);
+                transition.startTransition(500);
+            }
+        }, 2000, 2000);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        backgroundTimer.cancel();
     }
 
     private void open(View view, int id) {
